ae553427a2ddd5f08b753e8a186fc23ea252dead
Armando Soriano <asoriano.dev@gmail.com>
2020-11-01 13:15:38 +0100

    clenaup

--- backend/src/bitcoin/bitcoin.controller.ts
+++ backend/src/bitcoin/bitcoin.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Currency } from 'technest-trial-shared/enum/currency.enum';
 
 import { Response } from '../shared/response.model';
 
 import { BitcoinService } from './shared/bitcoin.service';
-import { Currency } from '../../../shared/enum/currency.enum';
 
 @Controller('bitcoin')
 export class BitcoinController {
